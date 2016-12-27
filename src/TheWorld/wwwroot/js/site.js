7aef2f79c21f7a10e65b43d6ab18e55f8b402775
paradigma <paradigma@SERVER2016>
2016-12-27 15:19:38 +0100

    Add project files.

---
+++ src/TheWorld/wwwroot/js/site.js
@@ -0,0 +1,48 @@
+﻿
+//(function () {
+//    var ele = document.getElementById("username");
+//    ele.innerHTML = "Paradigma";
+
+//    var main = document.getElementById("main");
+//    main.onmouseenter = function () {
+//        main.style.backgroundColor = "#888";
+//    }
+
+//    main.onmouseleave = function () {
+//        main.style.backgroundColor = "";
+//    }
+//})();
+
+(function () {
+
+    var ele = $("#username");
+    ele.text("Paradigma");
+
+    var main = $("#main");
+    main.on("mouseenter", function () {
+        main.style = "backgroundColor: #888";
+    });
+
+    main.on("mouseleave", function () {
+        main.style = "";
+    });
+
+    var menuItems = $("ul.menu li a");
+    menuItems.on("click", function () {
+        var me = $(this);
+        alert(me.text());
+    })
+
+    var $sidebarAndWrapper = $("#sidebar, #wrapper");
+
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) 
+        {
+            $(this).text("Show Sidebar");
+        } else {
+            $(this).text("Hide Sidebar");
+        }
+    });
+
+})();
