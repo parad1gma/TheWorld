1ed7e77750c1b086430b9bb78d9b1a23f31fe526
paradigma <paradigma@SERVER2016>
2016-12-28 16:58:57 +0100

    Adding a Service

--- src/TheWorld/Controllers/Web/AppController.cs
+++ src/TheWorld/Controllers/Web/AppController.cs
@@ -1,14 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using TheWorld.Services;
+using TheWorld.ViewModels;
 
 namespace TheWorld.Controllers.Web
 {
     public class AppController : Controller
     {
+        private IMailService _mailService;
+        private IConfigurationRoot _config;
+
+        public AppController(IMailService mailService, IConfigurationRoot config)
+        {
+            _mailService = mailService;
+            _config = config;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -16,6 +23,14 @@ namespace TheWorld.Controllers.Web
 
         public IActionResult Contact()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            _mailService.SendMail(_config["MailSettings:Email"], model.Email, "From TheWorld", model.Message);
+
             return View();
         }
 
