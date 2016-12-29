19ab560e7dfcaf837af156c4e23e0a80dbf74942
paradigma <paradigma@SERVER2016>
2016-12-29 12:13:37 +0100

    Creating the DbContext

---
+++ src/TheWorld/Models/WorldContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TheWorld.Models
+{
+    public class WorldContext : DbContext
+    {
+        public WorldContext()
+        {
+
+        }
+
+        public DbSet<Trip> Trips { get; set; }
+        public DbSet<Stop> Stops { get; set; }
+    }
+}
