cf74c4c77f9acbe3e0f9a5c9ad478e40265f2a5e
paradigma <paradigma@SERVER2016>
2016-12-29 15:48:51 +0100

    Create an API Controller

---
+++ src/TheWorld/Controllers/Api/TripsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TheWorld.Models;
+
+namespace TheWorld.Controllers.Api
+{
+    public class TripsController : Controller
+    {
+        //[HttpGet("api/trips")]
+        //public JsonResult Get()
+        //{
+        //    return Json(new Trip() { Name = "My Trip"});
+        //}
+
+        [HttpGet("api/trips")]
+        public IActionResult Get()
+        {
+            //if (true) return BadRequest("Bad things happened");
+
+            return Ok(new Trip() { Name = "My Trip" });
+        }
+    }
+}
